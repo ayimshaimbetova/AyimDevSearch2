d26e3082b592b7647dd7a44f9839b789cc7188ca
Ayim <ayim.shaimbetova@alatoo.edu.kg>
2023-11-03 11:46:29 +0600

    urls,views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+
+
+def register(request):
+    return HttpResponse('This is registration page!')
+
+def my_login(request):
+    return HttpResponse('this is the ligi page')
+
+def home(request):
+    return HttpResponse('Home page')
